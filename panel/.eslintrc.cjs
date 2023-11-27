9b33f1a471792cb681c883fc4cdcf296ce216ca0
Muhammad Syahrul <48862920+msyahrul801@users.noreply.github.com>
2023-11-27 23:37:00 +0700

    Update ESLint configuration

--- panel/.eslintrc.cjs
+++ panel/.eslintrc.cjs
@@ -4,7 +4,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["eslint:recommended", "plugin:vue/vue3-essential", "plugin:prettier/recommended"],
+  extends: ["@nuxt/eslint-config", "plugin:prettier/recommended"],
   overrides: [
     {
       env: {
